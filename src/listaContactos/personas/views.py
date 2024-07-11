0fb46d10f3f3ce5549e3e128bdb7c00f0beae62d
DrN25 <rninacal@unsa.edu.pe>
2024-07-10 22:16:11 -0500

    Diapositiva: Diferenciando llamada POST y GET. Se hicieron casos de llamada tanto de GET y POST en la vista personasAnotherCreateView

--- src/listaContactos/personas/views.py
+++ src/listaContactos/personas/views.py
@@ -22,7 +22,9 @@ def searchForHelp(request):
     return render(request, 'personas/search.html', {})
 
 def personasAnotherCreateView(request):
-    form = RawPersonaForm(request.POST)
+    form =  RawPersonaForm()
+    if request.method == "POST":
+        form = RawPersonaForm(request.POST)
     context = {
         'form': form,
     }
